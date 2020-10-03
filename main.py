1ace99933f449aaaa14bbc923934bed8535ec1cd
diegogliarte <63498452+diegogliarte@users.noreply.github.com>
2020-10-03 11:17:18 +0200

    Added sleep

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import whatsapp
 import rain
 import os
+from time import sleep
 
 def main():
     print("Beginning rain-notification.")
     stats = rain.get_rain("http://www.aemet.es/xml/municipios_h/localidad_h_25120.xml")
     messages = []
     for key, value in stats.items():
-        if value[0].count("lluvia") or int(value[1]) >= 10:
+        if value[0].count("lluvia") or int(value[1]) >= 40:
             messages.append(f"Around {key}:00h, there's a {value[1]}% of raining. The description is '{value[0]}.'")
 
     sender = os.environ["TWILIO_SENDER"]
@@ -16,7 +17,7 @@ def main():
     for message in messages:
         whatsapp.send_whatsapp(message, sender, receiver)
         whatsapp.send_whatsapp(message, sender, receiver2)
-
+        sleep(1)
 if __name__ == "__main__":
     main()
 
